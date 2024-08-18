a2ee17262822558fd09f34ffe0b5b01e161da392
4Scope <agent4scope@gmail.com>
2024-08-18 21:34:15 +0100

    navigation markup, styling and responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,40 @@
+import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import { useState } from "react";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
+import { HamburgerMenu } from "./design/Header";
+import MenuSvg from "../assets/svg/MenuSvg";
+import Button from "./Button";
 
 const Header = () => {
+	const pathName = useLocation();
+	const [openNav, setOpenNav] = useState(false);
+
+	const toggleNav = () => {
+		if (openNav) {
+			enablePageScroll();
+			setOpenNav(false);
+		} else {
+			disablePageScroll();
+			setOpenNav(true);
+		}
+	};
+
+	const handleclick = () => {
+		if (!openNav) return;
+
+		enablePageScroll();
+		setOpenNav(false);
+	};
+
 	return (
-		<div className='fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm'>
+		<div
+			className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 ${
+				openNav ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+			}`}
+		>
 			<div className='flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
 				<a href='#hero' className='block w-[12rem] xl:mr-8'>
 					<img
@@ -14,21 +45,51 @@ const Header = () => {
 					/>
 				</a>
 
-				<nav className='hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
-					<div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
+				<nav
+					className={`${
+						openNav ? "flex" : "hidden"
+					} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+				>
+					<div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row w-full h-full'>
 						{navigation.map((item) => (
 							<a
+								onClick={handleclick}
 								href={item.url}
 								className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
 									item.onlyMobile ? "lg:hidden" : ""
-								} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+								} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+									item.url === pathName.hash
+										? "z-2 lg:text-n-1"
+										: "lg:text-n-1/50"
+								} leading-5 lg:hover:text-n-1 xl:px-12`}
 								key={item.id}
 							>
 								{item.title}
 							</a>
 						))}
+
+						<HamburgerMenu />
 					</div>
 				</nav>
+
+				<a
+					href='#signup'
+					className='button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'
+				>
+					New account
+				</a>
+
+				<Button className='hidden lg:flex' href='#login'>
+					Sign in
+				</Button>
+
+				<Button
+					className='ml-auto lg:hidden '
+					px='px-3'
+					onClick={toggleNav}
+				>
+					<MenuSvg openNavigation={openNav} />
+				</Button>
 			</div>
 		</div>
 	);
