9f8254dad59b71ba1857b6c8c0d58d1cd0a607c2
4Scope <agent4scope@gmail.com>
2024-08-27 19:29:19 +0100

    pricing markup

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,9 +1,39 @@
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
 import Section from "./Section";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
 	return (
-		<Section>
-			<div className='container'>Pricing</div>
+		<Section className='overflow-hidden' id='pricing'>
+			<div className='container relative z-2'>
+				<div className='hidden relative justify-center mb-[6.5rem] lg:flex'>
+					<img
+						src={smallSphere}
+						className='relative z-1'
+						width={225}
+						height={255}
+						alt='sphere'
+					/>
+					<div className='absolute'>
+						<img
+							src={stars}
+							className='w-full'
+							width={950}
+							height={400}
+							alt='stars'
+						/>
+					</div>
+				</div>
+				<Heading
+					title='Pay once, use forever'
+					tag='Get started with Brainwave'
+				/>
+				<PricingList />
+				<LeftLine />
+				<RightLine />
+			</div>
 		</Section>
 	);
 };
