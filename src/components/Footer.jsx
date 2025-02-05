bb90a1dd814e2c37989874b9c05e481917d74455
Central Scope <agent4scope@gmail.com>
2025-02-05 06:38:06 +0100

    Footer markup and finalization of brainwave

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Section from "./Section";
+import { socials } from '../constants';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <Section crosses >
+      <div className='container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col'>
+
+        <p className='caption text-n-4  lg:block'>C {new Date().getFullYear()}. All rights reserved.</p>
+
+        <ul className='flex gap-5 flex-wrap'>
+          {socials.map(item => {
+            <a href={item.url} key={item.id} target='_blank' className='flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-0'>
+              <img src={item.iconUrl} alt={item.title} width={16} height={16}/>
+            </a>
+          })}
+        </ul>
+        
+      </div>
+    </Section>
   )
 }
 
